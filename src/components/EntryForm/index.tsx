c9de643af8979d7b39542945dfd9474ba3a5e235
stjuks <stjuks@ttu.ee>
2020-03-25 17:47:14 +0200

    Add hotkeys hook

--- src/components/EntryForm/index.tsx
+++ src/components/EntryForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useContext } from 'react';
-import { Formik, FormikHelpers, FormikProps } from 'formik';
+import { Formik, FormikHelpers, FormikProps, useFormikContext } from 'formik';
 
 import { EntryFormContainer, MainFormFields } from './styles';
 import * as yup from 'yup';
@@ -10,6 +10,7 @@ import { observer } from 'mobx-react-lite';
 import SelectInput from '../SelectInput';
 import RepForm from '../RepForm';
 import { RepType } from '@/types';
+import { useHotkeys } from '@/util/hooks';
 
 interface EntryFormValues {
   from: string;
@@ -31,76 +32,14 @@ const validationSchema = yup.object({
   content: yup.string()
 });
 
-const UP_ARROW = 38;
-const CTRL = 18;
-const CMD = 91;
-const DOWN_ARROW = 40;
-const M_KEY = 77;
-
 const repOptions: RepType[] = ['MIST', 'CONTACTREP', 'JAMREP', 'INTREP'];
-/* {
-    type: 'MIST',
-    mechanism: '',
-    injuries: '',
-    signs: {
-      a: '',
-      b: '',
-      c: '',
-      d: '',
-      e: ''
-    },
-    treatment: ''
-  },
-  {
-    type: 'CONTACTREP'
-  },
-  {
-    type: 'JAMREP'
-  },
-  {
-    type: 'INTREP'
-  }
-]; */
 
 const EntryForm: React.FC = observer(() => {
   const diaryStore = useContext(DiaryStoreContext);
-  const contentInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (entry, formikHelpers: FormikHelpers<any>) => {
     diaryStore.addEntry(entry);
     formikHelpers.resetForm();
-
-    if (contentInputRef && contentInputRef.current) {
-      contentInputRef.current.focus();
-    }
-  };
-
-  const selectRecipientsWithArrows = (
-    keyMap: { [key: number]: boolean },
-    form: FormikProps<any>,
-    event: React.KeyboardEvent
-  ) => {
-    const diary = diaryStore.activeDiary;
-
-    if (diary) {
-      const { entries } = diary;
-
-      const hasEntries = entries.length > 0;
-      if (keyMap[DOWN_ARROW]) {
-        form.setValues(initialValues);
-      } else if (keyMap[M_KEY] && (keyMap[CMD] || keyMap[CTRL])) {
-        event.preventDefault();
-        if (form.values.rep.type === 'MIST')
-          form.setValues({ ...form.values, rep: { type: undefined } });
-        else form.setValues({ ...form.values, rep: { type: repOptions[0] } });
-      } else if (hasEntries && keyMap[UP_ARROW] && (keyMap[CMD] || keyMap[CTRL])) {
-        const { from, to } = entries[entries.length - 1];
-        form.setValues({ ...form.values, from: to, to: from });
-      } else if (hasEntries && keyMap[UP_ARROW]) {
-        const { from, to } = entries[entries.length - 1];
-        form.setValues({ ...form.values, from, to });
-      }
-    }
   };
 
   return (
@@ -111,45 +50,83 @@ const EntryForm: React.FC = observer(() => {
       validateOnChange={false}
       onSubmit={handleSubmit}
     >
-      {formikProps => (
-        <EntryFormContainer onSubmit={formikProps.handleSubmit}>
-          <MainFormFields>
-            <div className="short-fields">
-              <SelectInput
-                label="REP"
-                name="rep.type"
-                options={repOptions}
-                optionLabel={option => option}
-                style={{ width: '5rem' }}
-              />
-              <TextInput
-                name="to"
-                label="Kellele"
-                style={{ width: '5rem' }}
-                onKeyPress={selectRecipientsWithArrows}
-              />
-              <TextInput
-                name="from"
-                label="Kellelt"
-                style={{ width: '5rem' }}
-                onKeyPress={selectRecipientsWithArrows}
-              />
-            </div>
-            <div className="content-field">
-              <TextInput
-                name="content"
-                ref={contentInputRef}
-                label="Sisu"
-                style={{ flex: 1 }}
-                onKeyPress={selectRecipientsWithArrows}
-              />
-            </div>
-          </MainFormFields>
-          <RepForm type={formikProps.values.rep?.type} />
-          <Button type="submit" title="Loo sissekanne" />
-        </EntryFormContainer>
-      )}
+      <EntryFormComponent />
     </Formik>
+  );
+});
+
+const EntryFormComponent: React.FC = observer(() => {
+  const diaryStore = useContext(DiaryStoreContext);
+  const { handleSubmit, values, setValues, resetForm } = useFormikContext<any>();
+  const contentInputRef = useRef<HTMLInputElement>(null);
+
+  const focusContentInput = () => {
+    if (contentInputRef && contentInputRef.current) {
+      contentInputRef.current.focus();
+    }
+  };
+
+  const onSubmit = event => {
+    handleSubmit(event);
+    focusContentInput();
+  };
+
+  const displayMist = () => {
+    if (values.rep?.type === 'MIST') setValues({ ...values, rep: { type: undefined } });
+    else setValues({ ...values, rep: { type: repOptions[0] } });
+  };
+
+  const selectPreviousRecipients = (switchRecipients?: boolean) => {
+    const diary = diaryStore.activeDiary;
+
+    focusContentInput();
+
+    if (diary) {
+      const { entries } = diary;
+
+      const lastEntry = entries[entries.length - 1];
+
+      if (lastEntry) {
+        const { from, to } = lastEntry;
+        if (switchRecipients) setValues({ ...values, from: to, to: from });
+        else setValues({ ...values, from, to });
+      }
+    }
+  };
+
+  useHotkeys(
+    {
+      'shift+m': displayMist,
+      'shift+arrowup': () => selectPreviousRecipients(true),
+      arrowup: selectPreviousRecipients,
+      arrowdown: resetForm
+    },
+    [values]
+  );
+
+  return (
+    <>
+      <EntryFormContainer onSubmit={onSubmit}>
+        <MainFormFields>
+          <div className="short-fields">
+            <SelectInput
+              label="REP"
+              name="rep.type"
+              options={repOptions}
+              optionLabel={option => option}
+              style={{ width: '5rem' }}
+            />
+            <TextInput name="to" label="Kellele" style={{ width: '5rem' }} />
+            <TextInput name="from" label="Kellelt" style={{ width: '5rem' }} />
+          </div>
+          <div className="content-field">
+            <TextInput name="content" ref={contentInputRef} label="Sisu" style={{ flex: 1 }} />
+          </div>
+        </MainFormFields>
+        <RepForm type={values.rep?.type} />
+        <Button type="submit" title="Loo sissekanne" />
+      </EntryFormContainer>
+    </>
   );
 });
 
--- src/util/hooks.ts
+++ src/util/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState, useCallback, useMemo } from 'react';
 
 export const usePrevious = value => {
   const ref = useRef();
@@ -8,4 +8,59 @@ export const usePrevious = value => {
   });
 
   return ref.current;
+};
+
+interface KeybindMap {
+  [key: string]: () => any;
+}
+
+export const useHotkeys = (keyBinds: KeybindMap, deps: any[]) => {
+  const [hotkeys, setHotkeys] = useState<KeybindMap>({});
+  const [pressedKeys, setPressedKeys] = useState<string[]>([]);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      const key = event.key.toLowerCase();
+
+      const tempKeyString = [...pressedKeys, key].join('+');
+      if (hotkeys[tempKeyString]) event.preventDefault();
+
+      if (pressedKeys.indexOf(key) === -1) {
+        setPressedKeys(keys => [...keys, key]);
+      }
+    },
+    [pressedKeys]
+  );
+
+  useEffect(() => {
+    const keyString = pressedKeys.join('+');
+
+    if (hotkeys[keyString]) {
+      hotkeys[keyString]();
+    }
+  }, [pressedKeys]);
+
+  useEffect(() => {
+    const hotkeyMap = {};
+
+    Object.entries(keyBinds).forEach(([key, value]) => {
+      key.split(',').forEach(bind => (hotkeyMap[bind] = value));
+    });
+
+    setHotkeys(hotkeyMap);
+  }, deps);
+
+  useEffect(() => {
+    const handleKeyUp = () => {
+      if (pressedKeys.length !== 0) setPressedKeys([]);
+    };
+
+    window.addEventListener('keyup', handleKeyUp);
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp);
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 };
